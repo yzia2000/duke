91562f184bac7c56b91b59e768d987f93f01497d
Mohammad Yousuf Minhaj Zia <e0381439@u.nus.edu>
2019-09-03 16:12:27 +0800

    changed current implementation to use ArrayList

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 package seedu.duke;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.BufferedWriter;
@@ -9,37 +10,34 @@ import java.io.IOException;
 
 public class Duke {
     // Utilize Task class
-    protected static Task list[] = new Task[100];
+    protected static ArrayList<Task> list = new ArrayList<Task>(); 
 
-    // length of initialized tasks
-    protected static int list_len;
-    
     // Loads duke.txt
     public static void load() {
       try {
         Scanner duke_txt = new Scanner(new File("../../../data/duke.txt"));
-        int index = 0;
-        while (duke_txt.hasNextLine() && index < 100) {
+        /* int index = 0; */
+        while (duke_txt.hasNextLine()) {
           // splits line input based on |
           String task_string[] = duke_txt.nextLine().split("\\|");
           
           // instantiate classes
           if (task_string[0].equals("T")) {
-            list[index] = new ToDo(task_string[2]);
+            list.add(new ToDo(task_string[2]));
           }
           else if (task_string[0].equals("D")) {
-            list[index] = new Deadline(task_string[2], task_string[3]);
+            list.add(new Deadline(task_string[2], task_string[3]));
           }
           else {
-            list[index] = new Event(task_string[2], task_string[3]);
+            list.add(new Event(task_string[2], task_string[3]));
           }
 
           if (task_string[1].equals("1")) {
-           list[index].markAsDone();
+           list.get(list.size() - 1).markAsDone();
           }
-          index++;
+          /* index++; */
         }
-        list_len = index + 1;
+        /* list_len = index + 1; */
       }
       catch (FileNotFoundException e) {
         System.out.println("\t_____________________________________");
@@ -50,7 +48,7 @@ public class Duke {
 
     public static void save() throws IOException {
       // if list has nothing just quit
-      if (list[0] == null) {
+      if (list.isEmpty()) {
         return;
       }
       
@@ -61,12 +59,12 @@ public class Duke {
       }
       
       // save inputs
-      String saved_line = list[0].toSaveFormat();
-      for (int i = 1; i < 100; i++) {
-        if (list[i] == null) {
-          break;
-        }
-        saved_line = saved_line + "\n" + list[i].toSaveFormat();
+      String saved_line = list.get(0).toSaveFormat();
+      for (Task task : list) {
+        /* if (list[i] == null) { */
+        /*   break; */
+        /* } */
+        saved_line = saved_line + "\n" + task.toSaveFormat();
       }
       BufferedWriter writer = new BufferedWriter(new FileWriter(new File("../../../data/duke.txt")));
       writer.write(saved_line);
@@ -81,7 +79,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
       System.out.println("Hello from\n" + logo);
 
-      list_len = 1;
+      /* list_len = 1; */
 
       Scanner input = new Scanner(System.in);
 
@@ -92,10 +90,10 @@ public class Duke {
       // Taking the first word as input for task type
       String task_type = input.next();
       
-      int index = list_len - 1;
+      int index = list.size() - 1;
 
       // Taking input and printing till user input is bye or the list hits 100
-      while (!task_type.equals("bye") && index < 100) {
+      while (!task_type.equals("bye")) {
         // If user inputs list
         if (task_type.equals("list")) {
           displayList();
@@ -110,7 +108,7 @@ public class Duke {
           // if tasktype is todo initialize object of class ToDo
           if (task_type.equals("todo")) {
             String task_description_full = input.nextLine().substring(1);
-            list[index] = new ToDo(task_description_full);
+            list.add(new ToDo(task_description_full));
 
             try {
               save();  
@@ -128,7 +126,7 @@ public class Duke {
                 String task_description_full = input.nextLine().substring(1);
                 String task_description = task_description_full.split("/", 2)[0];
                 String task_time = task_description_full.split("/", 2)[1].substring(3);
-                list[index] = new Deadline(task_description, task_time);
+                list.add(new Deadline(task_description, task_time));
                 try {
                   save();  
                 } catch(IOException e){
@@ -153,7 +151,7 @@ public class Duke {
                 String task_description_full = input.nextLine().substring(1);
                 String task_description = task_description_full.split("/", 2)[0];
                 String task_time = task_description_full.split("/", 2)[1].substring(3);
-                list[index] = new Event(task_description, task_time);
+                list.add(new Event(task_description, task_time));
                 try {
                   save();  
                 } catch(IOException e){
@@ -177,9 +175,8 @@ public class Duke {
           // Printing user input
           try {
             // The output to print on writing correct command
-            String output = "\t  " + list[index].toString();
+            String output = "\t  " + list.get(list.size() - 1).toString();
             index++;
-            list_len = index;
             System.out.println("\t_____________________________________");
             System.out.println("\tGot it. I've added this task:");
             System.out.println(output);  
@@ -206,7 +203,7 @@ public class Duke {
 
     public static void displayList() {
       // If user inputs list without appending list even once.
-      if (list[0] == null) {
+      if (list.isEmpty()) {
         System.out.println("\t_____________________________________");
         System.out.println("\tList is empty. Please type another command apart from list.");
         System.out.println("\t_____________________________________\n\n");
@@ -215,18 +212,15 @@ public class Duke {
 
       System.out.println("\t_____________________________________");
       System.out.println("\tHere are the tasks in your list:");
-      for (int i = 0; i < 100; i++) {
-        if (list[i] == null) {
-          break;
-        }
-        System.out.println("\t" + (i + 1) + "." + list[i].toString());
+      for (int i = 0; i < list.size(); i++) {
+        System.out.println("\t" + (i + 1) + "." + list.get(i).toString());
       } 
       System.out.println("\t_____________________________________\n\n");
     }
 
     public static void doTask(int i) {
       try {
-        list[i - 1].markAsDone();
+        list.get(i - 1).markAsDone();
         try {
           save();  
         } catch(IOException e){
@@ -236,7 +230,7 @@ public class Duke {
         }
         System.out.println("\t_____________________________________");
         System.out.println("\tNice! I've marked this task as done:");
-        System.out.println("\t  " + (i) + "." + list[i - 1].toString());
+        System.out.println("\t  " + (i) + "." + list.get(i - 1).toString());
         System.out.println("\t_____________________________________\n\n");
       }
       // Catch exception if wrong task id is done
